5e482b67dffd48664805dda9630c8b856a856f98
Roberto <roberto@rezende.biz>
2024-03-10 08:01:22 -0300

    Criar fila para notificacoes de movimentacao

--- scripts/criar_ambiente_rabbitmq.py
+++ scripts/criar_ambiente_rabbitmq.py
@@ -2,8 +2,11 @@
 ###############################################################################
 #
 # Script para criar o ambiente do RabbitMQ
-# Parte do projeto do módulo Soluções Nuvem do treinamento Jornada Digital 
+#
+# Parte do projeto do módulo Serviços Cloud do treinamento Jornada Digital 
 # ADA-Caixa
+#
+# Autor: Roberto Flavio Rezende
 #
 from rabbitmq_connection import new_connection
 from datetime import datetime
@@ -12,10 +15,16 @@ from datetime import datetime
 print(f'{datetime.now()} Criando ambiente...')
 con = new_connection()
 channel = con.channel()
+
 channel.exchange_declare(exchange='transacoes', exchange_type='direct')
+
+# Fila para as transações
 channel.queue_declare(queue='transacoes_solicitadas', durable=True)
-# channel.queue_declare(queue='transacoes_efetivadas', durable=True)
 channel.queue_bind(queue='transacoes_solicitadas', exchange='transacoes', routing_key='solicitar')
-# channel.queue_bind(queue='transacoes_efetivadas', exchange='transacoes', routing_key='efetivar')
+
+# Fila para as notificações de movimentações em conta
+channel.queue_declare(queue='transacoes_notificacao', durable=True)
+channel.queue_bind(queue='transacoes_notificacao', exchange='transacoes', routing_key='notificar')
+
 con.close()
 print(f'{datetime.now()} Ambiente criado.')
